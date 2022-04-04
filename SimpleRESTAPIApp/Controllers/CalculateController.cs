40f9dae2478edf7969b52e9ae8556dd36b6f9535
biruk14 <beck132619@gmail.com>
2022-04-04 16:27:39 +0300

    initial commit

---
+++ SimpleRESTAPIApp/Controllers/CalculateController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SimpleRESTAPIApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculateController : ControllerBase
+    {
+        private double _valueOne;
+        private double _valueTwo;
+
+        public CalculateController(double valueOne, double valueTwo)
+        {
+            this._valueOne = valueOne;
+            this._valueTwo = valueTwo;
+        }
+        // GET: api/<CalculateController>
+        [HttpGet]
+        public ActionResult<double> GetSum()
+        {
+            var opration = new Opration(_valueOne, _valueTwo).Sum();
+            return opration;
+        }
+        [HttpGet]
+        public ActionResult<double> GetSubtract()
+        {
+            var opration = new Opration(_valueOne, _valueTwo).Subtract();
+            return opration;
+        }
+        [HttpGet]
+        public ActionResult<double> GetMultiply()
+        {
+            var opration = new Opration(_valueOne, _valueTwo).Multiply();
+            return opration;
+        }
+        [HttpGet]
+        public ActionResult<double> GetDivide()
+        {
+            var opration = new Opration(_valueOne, _valueTwo).Divide();
+            return opration;
+        }
+    }
+}
---
+++ SimpleRESTAPIApp/Controllers/Opration.cs
@@ -0,0 +1,30 @@
+﻿namespace SimpleRESTAPIApp.Controllers
+{
+    public class Opration
+    {
+        private double _valueone;
+        private double _valueTwo;
+        public Opration(double valueOne, double valueTwo)
+        {
+            this._valueone = valueOne;
+            this._valueTwo = valueTwo;
+        }
+        internal double Sum()
+        {
+            return this._valueone + this._valueTwo;
+        }
+
+        internal double Subtract()
+        {
+            return this._valueone - this._valueTwo;
+        }
+        internal double Multiply()
+        {
+            return this._valueone * this._valueTwo;
+        }
+        internal double Divide()
+        {
+            return this._valueone / this._valueTwo;
+        }
+    }
+}
